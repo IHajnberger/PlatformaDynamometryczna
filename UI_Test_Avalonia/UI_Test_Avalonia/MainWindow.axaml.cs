1629b553f2b25a7d7de157d671b3ab9a5360774e
Mati-Muli <m.mulewicz89@gmail.com>
2026-05-13 13:48:20 +0200

    Templatka

---
+++ UI_Test_Avalonia/UI_Test_Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace UI_Test_Avalonia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

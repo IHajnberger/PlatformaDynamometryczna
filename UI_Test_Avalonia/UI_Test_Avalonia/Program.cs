3e521467658b329b10bb9c010640e08d4ad636b4
devoid-of-thought <mikynowy@gmail.com>
2026-05-14 01:20:34 +0200

    feat: got Live data working

--- UI_Test_Avalonia/UI_Test_Avalonia/Program.cs
+++ UI_Test_Avalonia/UI_Test_Avalonia/Program.cs
@@ -16,9 +16,6 @@ namespace UI_Test_Avalonia
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
-#if DEBUG
-                .WithDeveloperTools()
-#endif
                 .WithInterFont()
                 .LogToTrace();
     }
